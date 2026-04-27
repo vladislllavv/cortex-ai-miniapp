300a0e1d2fb5788ac31bf1232b3e2d4bf0a59163
vladislllavv <147401482+vladislllavv@users.noreply.github.com>
2026-04-27 15:47:24 +0300

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASx0saii-gMIHmCC_e3i7gAIqL6NxUMZ4",
+  authDomain: "cortexai-65075.firebaseapp.com",
+  projectId: "cortexai-65075",
+  storageBucket: "cortexai-65075.firebasestorage.app",
+  messagingSenderId: "417649566965",
+  appId: "1:417649566965:web:f36c20a9e7af9a77da91bc"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
