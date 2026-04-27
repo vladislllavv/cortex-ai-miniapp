5bbe0a96b0694710ca4c335cde76d83e841c2096
vladislllavv <147401482+vladislllavv@users.noreply.github.com>
2026-04-27 14:42:22 +0300

    Initialize Telegram bot with start command

---
+++ bot.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+console.log("Бот запущен ✅");
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Бот работает ✅");
+});
