b26a2513c4cca19fdbadeea7b4036f8fb5424aa9
vladislllavv <147401482+vladislllavv@users.noreply.github.com>
2026-05-13 17:13:15 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { WorkspaceProvider } from "./contexts/WorkspaceContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <WorkspaceProvider>
+        <App />
+      </WorkspaceProvider>
     </ThemeProvider>
   </StrictMode>
 );
